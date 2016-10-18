6cb0b50d13b93a2a8f721ce29f56ede12999783c
Benjamin Welsh <benjaminwelsh2@gmail.com>
2016-10-18 12:57:22 -0400

    Moved domain loader into domainModel.py. It is now contained in the .load() method  of DomainModel. (See issue #15)

--- Project/domainModel.py
+++ Project/domainModel.py
@@ -1,21 +1,49 @@
 class DomainModel:
-    def __init__(self, individualList):
+    def __init__(self, individualList=[],path=""):
         self.individualList = individualList
+        if path:
+            self.load(path)
 
     def __repr__(self):
         return(self.individualList.__repr__())
 
+    def load(self,path):
+        # Initializing the Individuals List
+        indList = []
+
+        fileInput = open(path, "r")
+        lines = fileInput.readlines()
+        header = lines[0]
+        keys = header.split(",")
+        data = lines[1:]
+        for line in data:
+
+            if (line[-1]) == '\n': #Strip trailing newline
+                line = line[0:-1]
+
+            values = line.split(",")
+            tags={}
+            for k, v in zip(keys, values):
+                if v:
+                    tags[k]=v
+            self.individualList.append(Individual(values[0], values[1], tags))
+
+        fileInput.close()
+
+        # Initializing the Categories List and the Domain Model
+        # catList = ["Porifera", "Cnidaria", "Platyhelminthes", "Nematoda", "Mollusca", "Annelida", "Arthropoda", "Echinodermata", "Chordata"]
+        # random.shuffle(catList)
+
+        # Create domModel
 
 
 
 class Individual:
-    def __init__(self, name, category, imagepath):
+    def __init__(self, name, imagepath, tags):
         self.name = name
-        self.category = category
         self.imagepath = imagepath
 
-        self.tags={}
-        self.tags["category"]=category
+        self.tags=tags
 
     def __repr__(self):
         return(self.name+" "+self.imagepath+" "+str(self.tags))
--- Project/main.py
+++ Project/main.py
@@ -7,40 +7,13 @@ WINDOWWIDTH = 1500
 WINDOWHEIGHT = 1000
 WHITE = (255, 255, 255)
 
-def createDomainModel(fileName):
-        #Initializing the Individuals List
-        indList = []
-
-        fileInput = open(fileName, "r")
-        lines = fileInput.readlines()
-        lines = lines[1:]
-        for line in lines:
-
-            if(line[len(line)-1]) == '\n':
-                line = line[0:len(line)-1]
-
-            values = line.split(",")
-            indList.append(domainModel.Individual(values[0], values[1], values[2]))
-
-        fileInput.close()
-
-        random.shuffle(indList)
-
-        #Initializing the Categories List and the Domain Model
-        #catList = ["Porifera", "Cnidaria", "Platyhelminthes", "Nematoda", "Mollusca", "Annelida", "Arthropoda", "Echinodermata", "Chordata"]
-        #random.shuffle(catList)
-
-        #Create domModel
-        domModel = domainModel.DomainModel(indList)
-        return domModel
-
 def main():
 
     pygame.init()
     pygame.display.set_caption('BioLab')
 
     #Create Domain Model
-    domModel = createDomainModel("Buildings Domain 8.csv")
+    domModel=domainModel.DomainModel(path="Buildings Domain 8.csv")
 
     #Ask for the User Name & Create Controller
     print("Enter User Name:")
