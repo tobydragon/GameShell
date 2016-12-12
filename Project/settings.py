6fed422fcb4c2214b588b975cbbb140b4e3e167d
Benjamin Welsh <benjaminwelsh2@gmail.com>
2016-12-12 14:07:05 -0500

    Minor settings change

--- Project/settings.py
+++ Project/settings.py
@@ -1,4 +1,4 @@
 LOG_SELECTIONS = False  # Log each time a user selects or deselects a card
 LOG_STAGE_EVALS = True  # Log each time a stage is evaluated
 DOMAIN_FILE = "insects_set.csv"
-STAGES_PER_QUESTION=1
+STAGES_PER_QUESTION=2
