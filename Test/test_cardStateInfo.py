44cb89606ee436ab4042a91e6eff680a32327a16
Benjamin Welsh <benjaminwelsh2@gmail.com>
2017-03-02 15:55:23 -0500

    Added unit test base

---
+++ Test/test_cardStateInfo.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from stageController import CardStateInfo
+
+class TestCardStateInfo(TestCase):
+
+
+    def buildExampleIntCardState(self):
+        selected=[1, 2, 3, 4]
+        unselected=[5, 6, 7, 8]
+        rightTag=[1, 2, 5, 6]
+        wrongTag=[3, 4, 7, 8]
+        return CardStateInfo(selected, unselected, rightTag, wrongTag)
+
+    def testConstructor(self):
+        intCardStateInfo = self.buildExampleIntCardState()
+
+        self.assertCountEqual([1,2], intCardStateInfo.correctRightTag)
+        self.assertCountEqual([7, 8], intCardStateInfo.correctWrongTag)
+        self.assertCountEqual([1, 2, 7, 8], intCardStateInfo.correct)
+        self.assertCountEqual([3, 4, 5, 6], intCardStateInfo.incorrect)
